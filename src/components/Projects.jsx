1df5e98e97a5bb9e5f32ff2f41f6c203cad9a4fb
SP UDAY KIRAN REDDY <udaykiranreddysp25@gmail.com>
2024-12-22 22:09:31 +0530

    updating projects path

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,16 +9,18 @@ const projects = [
     title: "Travel List",
     description:
       "It's Like a TO-DO app you can list what are the items you need pack for trip and you can note what all you already packed and how many you still need to pack.",
-    imageUrl: "https://shorturl.at/NH8YO",
-    link: "https://github.com/spudaykiran/travel-list",
+    imageUrl:
+      "https://tse1.mm.bing.net/th?id=OIG3.ZihbSC4KF9Fd_mJx6KkQ&pid=ImgGn",
+    link: "https://spudaykiran.github.io/travel-list/",
   },
   {
     id: 2,
     title: "usePopcorn",
     description:
       "It is a like movie platform you can search a movie and add to your watchlist and you can give rating to the movie. you can watch the cast of the movie also",
-    imageUrl: "https://shorturl.at/YGmqA",
-    link: "https://github.com/spudaykiran/usepopcorn",
+    imageUrl:
+      "https://tse3.mm.bing.net/th?id=OIG1.otIGW0Ve9OQn0r2O0MnL&pid=ImgGn",
+    link: "https://spudaykiran.github.io/use-popcorn/",
   },
   {
     id: 3,
@@ -26,7 +28,7 @@ const projects = [
     description:
       "It is a Quiz application where you can conduct quiz for the students and a timer for the quiz for each question you can set time what ever you want.",
     imageUrl: "https://shorturl.at/CXQQF",
-    link: "https://github.com/spudaykiran/react-quiz",
+    link: "https://spudaykiran.github.io/react-quiz-own",
   },
   {
     id: 4,
@@ -34,7 +36,7 @@ const projects = [
     description:
       "It is a app where you split the expenses between your friends and the friend will have a profile and who is paying the bill and split according to it.",
     imageUrl: "https://rb.gy/t34408",
-    link: "https://github.com/spudaykiran/eat-n-split",
+    link: "https://spudaykiran.github.io/eat-n-split/",
   },
   {
     id: 5,
@@ -42,7 +44,7 @@ const projects = [
     description:
       "It is an IOT Project designed in my college. The dustbin contains features like opening and closing automatically and it is also have RFID feature like whenever the dustbin is full the municipality person needs to use id card to open.",
     imageUrl: "https://rb.gy/tfbqlr",
-    link: "https://github.com/spudaykiran/SmartBin",
+    link: "https://github.com/spudaykiran/SmartBin/blob/main/P-1.ino",
   },
   {
     id: 6,
@@ -50,7 +52,7 @@ const projects = [
     description:
       "It is a weather app you can search about the weather of particular location it will next 7 days weather report in the particular place you asked for.",
     imageUrl: "https://rb.gy/hyoqe8",
-    link: "https://github.com/spudaykiran/classy-weather",
+    link: "https://spudaykiran.github.io/classy-weather/",
   },
   {
     id: 7,
